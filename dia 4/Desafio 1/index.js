4a67b6572df062cdab6e0aab92e90124cb979827
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-08-18 17:45:36 -0300

    Finaliza o desafio 1 do dia 4

---
+++ dia 4/Desafio 1/index.js
@@ -0,0 +1,11 @@
+let fome = prompt(" Você está com fome? digite 's' para sim e 'n' para não")
+let dinheiro = prompt(" Você tem dinheiro? digite 's' para sim e 'n' para não")
+let restaurante = prompt(" O restaurante está aberto? digite 's' para sim e 'n' para não")
+
+if(fome === 'n' || dinheiro === 'n' ) {
+    console.log("Hoje a janta será em casa")
+} else if (fome === 's' && dinheiro === 's' && restaurante === 'n') {
+    console.log("Peça um delivery!")
+} else if (fome === 's' && dinheiro === 's' && restaurante === 's' ) {
+    console.log("Hoje o jantar será no seu restaurante preferido!")
+}
