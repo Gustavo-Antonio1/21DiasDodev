3852c58ed7bd131d92dc5415cafd1f4199fef965
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-08-23 17:57:18 -0300

    Adiciona o algoritmo para extrair dados do usuario no desafio 1

---
+++ dia 9/Desafio 1/index.js
@@ -0,0 +1,7 @@
+let nome = prompt("Insira aqui o seu nome")
+let idade = Number(prompt("Insira aqui a sua idade"))
+let peso = Number (prompt("Insira aqui o seu peso em kg"))
+let altura = Number(prompt("Insira aqui a sua altura em M"))
+let profissao = prompt("Insira aqui a sua profissao")
+
+console.log("Ola " + nome + ", voce tem " + idade + " anos, é " + profissao + ", tem " + altura + "M de altura e pesa " + peso + "kg")
