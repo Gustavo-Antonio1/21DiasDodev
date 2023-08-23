1b27c0be0afa5faa64172d967f0639fdea4e4b7d
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-08-23 18:05:59 -0300

    Adiciona calculo de maioridade e calculo de idade por meses, semanas e dias

--- dia 9/Desafio 1/index.js
+++ dia 9/Desafio 1/index.js
@@ -5,3 +5,17 @@ let altura = Number(prompt("Insira aqui a sua altura em M"))
 let profissao = prompt("Insira aqui a sua profissao")
 
 console.log("Ola " + nome + ", voce tem " + idade + " anos, é " + profissao + ", tem " + altura + "M de altura e pesa " + peso + "kg")
+
+if(idade >= 18){
+    console.log("está liberado para tomar umas geladas")
+}else {
+    console.log("Sem gelada pra você")
+}
+
+let idadeEmMeses = idade * 12
+let idadeEmSemanas = idade * 48
+let idadeEmDias = idade * 365
+
+console.log("sua idade em meses é: " + idadeEmMeses + " meses")
+console.log("sua idade em semanas é: " + idadeEmSemanas + " semanas")
+console.log("sua idade em dias é: " + idadeEmDias + " dias")
