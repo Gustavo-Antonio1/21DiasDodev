e55d9a67501e58ed73df77d1c07cc0e954dd488b
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-09-01 11:32:27 -0300

    Finaliza exercicio do dia 15

--- dia 15/index.js
+++ dia 15/index.js
@@ -1,4 +1,23 @@
-let arrayA = [10,23,62,45,89,21,67,102,302,75]
-let arrayB = [10,56,34,89,67,22,102,75,199,20]
+let arrayA = [10, 23, 62, 45, 89, 21, 67, 102, 302, 75]
+let arrayB = [10, 56, 34, 89, 67, 22, 102, 75, 199, 20]
 let arrayC = [];
+let contadorArrayC = 0
+
+function CompareArrays(array1, array2) {
+
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                arrayC[contadorArrayC] = array1[i]
+                contadorArrayC++
+                j = array2.length
+            }
+        }
+
+    }
+    return arrayC
+}
+
+
+console.log(CompareArrays(arrayA, arrayB))
 
