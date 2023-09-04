d40cb63d96129995cbacc87d904fb0b3139e4798
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-09-04 10:55:13 -0300

    finaliza exercicio do dia 16

--- dia 16/index.js
+++ dia 16/index.js
@@ -1,9 +1,35 @@
 let nomes = [];
 let senhas = [];
+let opcao;
+let continuar = true
+
+while(continuar) {
+    EscolhaUsuario()
+
+    switch (opcao) {
+        case 1:
+            CadastrarUsuario()
+            break;
+        case 2:
+            LoginUsuario()
+            break;
+        case 3: 
+            ExcluirCadastro()
+            break;
+        case 4:
+            console.log("Sistema encerrado")
+            continuar = false
+            break;
+        default:
+            console.log("Acao invalida")
+            break;
+    }
+}
+
 
 
 function EscolhaUsuario() {
-    let opcao = Number(prompt("Insira o numero correspondente a acao desejada: \n1 - para Cadastrar\n2 - para fazer login\n3 - para excluir usuario\n4 para encerrar o programa"))
+    opcao = Number(prompt("Insira o numero correspondente a acao desejada: \n1 - para Cadastrar\n2 - para fazer login\n3 - para excluir usuario\n4 - para encerrar o programa"))
 
     return opcao;
 }
@@ -14,6 +40,8 @@ function CadastrarUsuario() {
 
     nomes.push(nomeInserido)
     senhas.push(senhaInserida)
+
+    console.log("Usuario cadastrado com sucesso")
 }
 
 function LoginUsuario(nome, senha) {
@@ -24,6 +52,18 @@ function LoginUsuario(nome, senha) {
         console.log("Login efetuado com sucesso")
     } else {
         console.log("login invalido")
+    }
+}
+
+function ExcluirCadastro(nome){
+    nome = prompt("Insira o nome do usuario a ser excluido")
+    let indice = nomes.indexOf(nome)
+    if(indice !== -1) {
+        nomes.splice(indice, 1)
+        senhas.splice(indice, 1)
+        console.log("Cadastro excluido com sucesso") 
+    }else {
+        console.log("Usuario nao encontrado")
     }
 }
 
