9c119112cbc39b0f2e756e47fd2dcc4e8598cab7
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-09-04 10:11:53 -0300

    Cria segunda funcao do exercicio para cadastrar usuarios

--- dia 16/index.js
+++ dia 16/index.js
@@ -1,5 +1,19 @@
+let nomes = [];
+let senhas = [];
+
+
 function EscolhaUsuario() {
     let opcao = Number(prompt("Insira o numero correspondente a acao desejada: \n1 - para Cadastrar\n2 - para fazer login\n3 - para excluir usuario\n4 para encerrar o programa"))
 
     return opcao;
 }
+
+function CadastrarUsuario () {
+    let nomeInserido = prompt("Por gentileza insira o nome de usuario.")
+    let senhaInserida = prompt("Por gentileza insira a senha desejada.")
+
+    nomes.push(nomeInserido)
+    senhas.push(senhaInserida)
+}
+
+CadastrarUsuario()
