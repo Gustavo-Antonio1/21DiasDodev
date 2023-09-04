744f576673214bf5cf3c9918468c3489c6e2cbb5
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-09-04 10:05:03 -0300

    Cria primeira funcao do exercicio para escolha do usuario

---
+++ dia 16/index.js
@@ -0,0 +1,5 @@
+function EscolhaUsuario() {
+    let opcao = Number(prompt("Insira o numero correspondente a acao desejada: \n1 - para Cadastrar\n2 - para fazer login\n3 - para excluir usuario\n4 para encerrar o programa"))
+
+    return opcao;
+}
